7660286e64f7685faef7087fdce5a4e90bf45e0b
boyproO19 <rupak.chaulagainO19@gmail.com>
2023-08-28 20:40:39 +0545

    Fluid.

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,28 +1,27 @@
-
 import "./HomePage.css";
 import "./HomePageMobile.css";
 export default function HomePage() {
   return (
     <div>
-      <head><meta name="viewport" content="width=device-width, initial-scale=1"/></head>
-      <div className="Hero">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <div className="Hero" style={{ height: window.innerHeight }}>
         <div id="HeroContainer">
-            <div id="HeroIntro">
+          <div id="HeroIntro">
             <div id="ThisIs">Hello This is</div>
             <div id="HeroName">Rupak Chaulagain</div>
+            <div id="ContactMe">
+              <a href="#">Contact Me</a>
             </div>
+          </div>
           <div id="HeroImageContainer">
-            {/* <img src="/Images/Background.jpg" alt="Background"></img> */}
+            <img src="/Images/Background.jpg" alt="Background"></img>
           </div>
         </div>
       </div>
 
-      <div className="HeroBlog">
-          THis is a thing
-          
-      </div>
+      <div className="HeroBlog">THis is a thing</div>
     </div>
   );
 }
-
-
